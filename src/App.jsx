b296c4f75a2f7e9b90224136c1dfc27038427c99
Nishant-kumar-yadav-10 <nishant12ts@gmail.com>
2026-06-13 16:35:26 +0530

    project xompleted

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,37 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const[add, setAdd]=useState('')
+  const[task,setTask]=useState('')
+  const[todos,setTodos]=useState([])
+
+  function addTodo(){
+    if(task.trim()===""){
+      return
+    }
+    setTodos([...todos,task])
+    setTask("")
+
+  }
+  function deleteTask(index){
+const updatedTodos=todos.filter((_,i)=>i!=index);
+setTodos(updatedTodos)
+  }
 
 
   return (
     <>
   <h1>todo</h1>
-  <input type='text' placeholder='type here' ></input>
-  <button>add</button>
+  <input type='text' placeholder='type here'onChange={(e)=>setTask(e.target.value)} value={task}></input>
+  <button onClick={addTodo}>add</button>
+  <ul>
+    {todos.map((todo,index)=>(
+      <li key={index}>{todo}<button onClick={()=>deleteTask(index)}>delete</button></li>
+  
+    ))
+
+  }
+  </ul>
+
    </>
   )
 }
